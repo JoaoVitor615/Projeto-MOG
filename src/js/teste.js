6425cb32ef30e2631cdda0e6be3c47d364f8a42c
RODRIGO COSTA <rodrigo.costa158@etec.sp.gov.br>
2022-02-24 19:12:01 -0300

    Dark-mode, botões e afins.

--- src/js/teste.js
+++ src/js/teste.js
@@ -1,18 +1,14 @@
 document.body.setAttribute('data-theme', 't-light')
 
-const button_changecolor = document.querySelector('.js-theme')
+const button = document.querySelector('.js-theme')
 
 const themes = {
   't-dark': 't-light',
   't-light': 't-dark',
 }
 
-document.querySelector('.ball').addEventListener('click', (e)=>{
-  e.target.classList.toggle('ball-move');
-});
-
-if (button_changecolor) {
-  button_changecolor.addEventListener('click', event => {
+if (button) {
+  button.addEventListener('click', event => {
     event.preventDefault()
     const currentTheme = document.body.dataset.theme
     document.body.setAttribute('data-theme', themes[currentTheme] || 't-light')
